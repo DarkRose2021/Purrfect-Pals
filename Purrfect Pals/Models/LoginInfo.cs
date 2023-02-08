8457653b528c3a75b0fef45cbd04fcbd6e3a82ee
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2023-02-07 18:51:12 -0700

    Got Models for Login information, and the pet bio's implemented.

---
+++ Purrfect Pals/Models/LoginInfo.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Purrfect_Pals.Models{
+
+    //Login Information
+
+    public class LoginInfo{
+
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginInfo(string username, string password){
+
+            this.Username = username;
+
+            this.Password = password;
+        
+        }
+
+    }
+
+}
