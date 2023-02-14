cc75987dd295ef4e6e0f5601621269602b2a2263
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-02-13 18:53:17 -0700

    working on darkmode

--- Purrfect Pals/wwwroot/js/site.js
+++ Purrfect Pals/wwwroot/js/site.js
@@ -4,7 +4,24 @@ var body = document.body;
 
 
 function ToggleDarkMode() {
-	nav.classList.toggle("navDarkMode");
-	h1.classList.toggle("headerDarkMode");
-	body.classList.toggle("darkmode");
+	localStorage.setItem("dark", darkOn ? "true" : "false");
+	nav.classList.add("navDarkMode");
+	body.classList.add("darkmode");
+	h1.classList.add("headerDarkMode");
+
+}
+if (darkOn) {
+	document.body.setAttribute("theme", "dark");
+	togButton.innerHTML = "Turn off dark mode.";
+}
+else {
+	document.body.setAttribute("theme", "light");
+	togButton.innerHTML = "Turn on dark mode.";
+}
+}
+
+var darkOn = false;
+function toggle() {
+	darkOn = !darkOn;
+	setTheme();
 }
