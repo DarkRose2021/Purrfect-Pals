423e426b2c5a06c8c1c0c1af6722ee82d093fd5e
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-02-15 09:36:49 -0700

    changed color palette

--- Purrfect Pals/wwwroot/js/site.js
+++ Purrfect Pals/wwwroot/js/site.js
@@ -7,17 +7,21 @@ function ToggleDarkMode() {
 	localStorage.setItem("dark", darkOn ? "true" : "false");
 
 	if (darkOn) {
-		nav.classList.remove("nav");
+		nav.classList.remove("navbarLightMode");
+		nav.classList.remove("border-bottom");
 		body.classList.remove("lightmode");
 		h1.classList.remove("headerLightMode");
 		nav.classList.add("navDarkMode");
+		nav.classList.add("borderDarkMode");
 		body.classList.add("darkmode");
 		h1.classList.add("headerDarkMode");
 	} else {
-		nav.classList.add("nav");
+		nav.classList.add("navbarLightMode");
+		nav.classList.add("border-bottom");
 		body.classList.add("lightmode");
 		h1.classList.add("headerLightMode");
 		nav.classList.remove("navDarkMode");
+		nav.classList.remove("borderDarkMode");
 		body.classList.remove("darkmode");
 		h1.classList.remove("headerDarkMode");
 	}
