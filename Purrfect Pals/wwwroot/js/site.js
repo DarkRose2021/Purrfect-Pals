bbd8b58beddf041246892af509399a0be7a25b2f
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-02-15 16:19:10 -0700

    fixed links for darkmode

--- Purrfect Pals/wwwroot/js/site.js
+++ Purrfect Pals/wwwroot/js/site.js
@@ -2,7 +2,16 @@
 var nav = document.getElementById("navbar");
 var h1 = document.getElementById("header");
 var a = document.getElementById("a");
-var h3 = document.getElementById("h3");
+var a2 = document.getElementById("a2");
+var a3 = document.getElementById("a3");
+var a4 = document.getElementById("a4");
+var a5 = document.getElementById("a5");
+var a6 = document.getElementById("a6");
+
+if (document.getElementById("h3")) {
+	var h3 = document.getElementById("h3");
+}
+
 
 darkOn = localStorage.getItem("dark") == "true" ? true : false;
 ToggleDarkMode();
@@ -11,33 +20,63 @@ function ToggleDarkMode() {
 	localStorage.setItem("dark", darkOn ? "true" : "false");
 
 	if (darkOn) {
+		if (document.getElementById("h3")) {
+			h3.classList.remove("h3LightMode");
+			h3.classList.add("h3Darkmode");
+		}
+
 		nav.classList.remove("navbarLightMode");
 		nav.classList.remove("border-bottom");
 		body.classList.remove("lightmode");
 		h1.classList.remove("headerLightMode");
-		h3.classList.remove("h3LightMode");
+		
 		a.classList.remove("linkLightMode");
+		a2.classList.remove("linkLightMode");
+		a3.classList.remove("linkLightMode");
+		a4.classList.remove("linkLightMode");
+		a5.classList.remove("linkLightMode");
+		a6.classList.remove("linkLightMode");
 
 		nav.classList.add("navDarkMode");
 		nav.classList.add("borderDarkMode");
 		body.classList.add("darkmode");
 		h1.classList.add("headerDarkMode");
-		h3.classList.add("h3Darkmode");
+		
 		a.classList.add("linkDarkMode");
+		a2.classList.add("linkDarkMode");
+		a3.classList.add("linkDarkMode");
+		a4.classList.add("linkDarkMode");
+		a5.classList.add("linkDarkMode");
+		a6.classList.add("linkDarkMode");
 	} else {
+		if (document.getElementById("h3")) {
+			h3.classList.remove("h3Darkmode");
+			h3.classList.add("h3LightMode");
+		}
+
 		nav.classList.add("navbarLightMode");
 		nav.classList.add("border-bottom");
 		body.classList.add("lightmode");
 		h1.classList.add("headerLightMode");
-		h3.classList.add("h3LightMode");
+		
 		a.classList.add("linkLightMode");
+		a2.classList.add("linkLightMode");
+		a3.classList.add("linkLightMode");
+		a4.classList.add("linkLightMode");
+		a5.classList.add("linkLightMode");
+		a6.classList.add("linkLightMode");
 
 		nav.classList.remove("navDarkMode");
 		nav.classList.remove("borderDarkMode");
 		body.classList.remove("darkmode");
 		h1.classList.remove("headerDarkMode");
-		h3.classList.remove("h3Darkmode");
+		
 		a.classList.remove("linkDarkMode");
+		a2.classList.remove("linkDarkMode");
+		a3.classList.remove("linkDarkMode");
+		a4.classList.remove("linkDarkMode");
+		a5.classList.remove("linkDarkMode");
+		a6.classList.remove("linkDarkMode");
 	}
 
 }
